c982239b2e7c7dd8bfd11eb8916265a2b3ebf2f3
Pratyush-Saxena <saxena18prats@gmail.com>
2022-09-07 15:52:24 +0530

    updated

--- airflow/dags/utils.py
+++ airflow/dags/utils.py
@@ -8,7 +8,11 @@ import psycopg2
 load_dotenv()
 
 conn = psycopg2.connect(
-    host="localhost", database="test", user="test", password="test", port="5432"
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT"),
 )
 
 
